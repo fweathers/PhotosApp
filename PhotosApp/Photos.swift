fea04ad50ae8e2f4e500b2f3787d9eb1a12429a7
Felicia Weathers <Fel.Weathers@gmail.com>
2016-05-01 17:23:21 -0400

    Create Photo class that contains information from placeholderAPI

---
+++ PhotosApp/Photos.swift
@@ -0,0 +1,34 @@
+//
+//  Photos.swift
+//  PhotosApp
+//
+//  Created by Felicia Weathers on 5/1/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    var id : Int!
+    var title : String!
+    var url : String!
+    var thumbnailUrl : String!
+    
+    init(data: NSDictionary) {
+        
+        self.id = data["id"] as! Int
+        
+        self.title = getStringFromJSON(data, key: "title")
+        self.url = getStringFromJSON(data, key: "url")
+        self.thumbnailUrl = getStringFromJSON(data, key: "thumbnailUrl")
+        
+    }
+    
+    func getStringFromJSON(data: NSDictionary, key: String) -> String {
+                
+        if let info = data[key] as? String {
+            return info
+        }
+        return ""
+    }
+}
