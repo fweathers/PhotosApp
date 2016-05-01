4625e879ec429a312cc529813a0fbb5c97ac2942
Felicia Weathers <Fel.Weathers@gmail.com>
2016-05-01 16:39:06 -0400

    Initial commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Felicia Weathers on 5/1/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
