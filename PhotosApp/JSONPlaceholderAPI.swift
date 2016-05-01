2e84af95e0bfec951e035a7a545944dd35fb7ff3
Felicia Weathers <Fel.Weathers@gmail.com>
2016-05-01 16:41:14 -0400

    Create JSONPlaceholderAPI group and JSONPlaceholderAPI file

---
+++ PhotosApp/JSONPlaceholderAPI.swift
@@ -0,0 +1,13 @@
+//
+//  JSONPlaceholderAPI.swift
+//  PhotosApp
+//
+//  Created by Felicia Weathers on 5/1/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import Foundation
+
+class JSONPlaceholderAPI {
+    // This class will contain logic to fetch placeholder photos
+}
